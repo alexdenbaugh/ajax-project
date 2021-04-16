641a41349139ae3eda86f3f6dfc55b5498711a36
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-04-16 15:54:45 -0700

    Remove all CSS properties that have no effect. #1

--- js/data.js
+++ js/data.js
@@ -15,15 +15,17 @@ var data = {
   response: null
 };
 
-// var responses = {
-//   correct: 'Correct!',
-//   incorrect: 'Incorrect',
-//   simplify: 'Please use "^" for exponents and use parenthesis when dividing polynomials.',
-//   factor: 'Please use "^" for exponents and use parenthesis when dividing polynomials.',
-//   derive: 'Please use "^" for exponents and use parenthesis when dividing polynomials.',
-//   integrate: 'Please use "^" for exponents and use parenthesis when dividing polynomials.',
-//   trig: 'Please use following format: sin(x), cos(x), tan(x), arcsin(x), arccos(x), arctan(x). Type “pi” if using pi. Do not include variables in problem.',
-//   log: 'Please use following format: log2(8). Do not include variables in problem. For log base 10, use log10(x).'
-// }
+// eslint-disable-next-line no-unused-vars
+var responses = {
+  correct: 'Correct!',
+  incorrect: 'Incorrect',
+  simplify: 'Please use "^" for exponents and use parenthesis when dividing polynomials.',
+  factor: 'Please use "^" for exponents and use parenthesis when dividing polynomials.',
+  derive: 'Please use "^" for exponents and use parenthesis when dividing polynomials.',
+  integrate: 'Please use "^" for exponents and use parenthesis when dividing polynomials.',
+  trig: 'Please use following format: sin(x), cos(x), tan(x), arcsin(x), arccos(x), arctan(x). Type “pi” if using pi. Do not include variables in problem.',
+  log: 'Please use following format: log2(8). Do not include variables in problem. For log base 10, use log10(x).'
+};
 
-// var typeCheck = ['arcsin', 'arccos', 'arctan', 'sin', 'cos', 'tan', 'log'];
+// eslint-disable-next-line no-unused-vars
+var typeCheck = ['arcsin', 'arccos', 'arctan', 'sin', 'cos', 'tan', 'log'];
--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function getNewtonData(type, problem) {
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     data.calculator.result = xhr.response.result;
-    // updateResult(xhr.status);
+    updateResult(xhr.status);
   });
   xhr.send();
 }
@@ -22,68 +22,72 @@ $formCalculator.addEventListener('submit', function () {
   data.calculator.type = $formCalculator.elements.type.value;
   data.calculator.problem = $formCalculator.elements.problem.value;
   data.calculator.result = null;
-  // formatCheck()
+  formatCheck();
   getNewtonData(data.calculator.type, data.calculator.problem);
 });
 
-// function updateResult(status) {
-//   if (status === 400 || (!data.calculator.result && data.calculator.result !== 0 && isNaN(data.calculator.result))) {
-//     $pResult.textContent = responses[data.calculator.type];
-//     $pResult.classList.remove('black-text');
-//     $pResult.classList.add('red-text');
-//   } else {
-//     $pResult.textContent = data.calculator.result;
-//   }
-// }
+function updateResult(status) {
+  if (status === 400 || (!data.calculator.result && data.calculator.result !== 0 && isNaN(data.calculator.result))) {
+    // eslint-disable-next-line no-undef
+    $pResult.textContent = responses[data.calculator.type];
+    $pResult.classList.remove('black-text');
+    $pResult.classList.add('red-text');
+  } else {
+    $pResult.textContent = data.calculator.result;
+  }
+}
 
-// function formatCheck() {
-//   var problem = data.calculator.problem;
-//   var check = false;
-//   for (var i = 0; i < typeCheck.length; i++) {
-//     if (problem.startsWith(typeCheck[i])) {
-//       check = true;
-//       break
-//     }
-//   }
-//   if (check === false) {
-//     return
-//   }
-//   if (data.calculator.type === 'log') {
-//     problem = problem.slice(3);
-//     problem = problem.slice(0, problem.length - 1);
-//     problem = problem.replace('(', '|');
-//   } else if (data.calculator.type === 'trig') {
-//     data.calculator.type = problem.slice(0, problem.indexOf('('));
-//     problem = problem.slice(problem.indexOf('(') + 1, problem.length - 1)
-//   }
-//   data.calculator.problem = problem
-// }
+function formatCheck() {
+  var problem = data.calculator.problem;
+  var check = false;
+  // eslint-disable-next-line no-undef
+  for (var i = 0; i < typeCheck.length; i++) {
+    // eslint-disable-next-line no-undef
+    if (problem.startsWith(typeCheck[i])) {
+      check = true;
+      break;
+    }
+  }
+  if (check === false) {
+    return;
+  }
+  if (data.calculator.type === 'log') {
+    problem = problem.slice(3);
+    problem = problem.slice(0, problem.length - 1);
+    problem = problem.replace('(', '|');
+  } else if (data.calculator.type === 'trig') {
+    data.calculator.type = problem.slice(0, problem.indexOf('('));
+    problem = problem.slice(problem.indexOf('(') + 1, problem.length - 1);
+  }
+  data.calculator.problem = problem;
+}
 
-// var $views = document.querySelectorAll('.view');
-// var $menu = document.querySelector('.hamburger-menu');
+var $views = document.querySelectorAll('.view');
+var $menu = document.querySelector('.hamburger-menu');
 
-// function viewChanger (dataView) {
-//   if (dataView === 'hide-menu') {
-//     $menu.classList.add('hidden')
-//   } else if (dataView === 'show-menu') {
-//     $menu.classList.remove('hidden')
-//   } else {
-//     for (var i = 0; i < $views.length; i++) {
-//       if ($views[i].getAttribute('data-view') === dataView) {
-//         $views[i].classList.remove('hidden');
-//       } else {
-//         $views[i].classList.add('hidden');
-//       }
-//     }
-//     $menu.classList.add('hidden')
-//   }
-// }
+function viewChanger(dataView) {
+  if (dataView === 'hide-menu') {
+    $menu.classList.add('hidden');
+  } else if (dataView === 'show-menu') {
+    $menu.classList.remove('hidden');
+  } else {
+    for (var i = 0; i < $views.length; i++) {
+      if ($views[i].getAttribute('data-view') === dataView) {
+        $views[i].classList.remove('hidden');
+      } else {
+        $views[i].classList.add('hidden');
+      }
+    }
+    $menu.classList.add('hidden');
+  }
+}
 
-// document.addEventListener('click', function() {
-//   if (!event.target.classList.contains('view-changer')) {
-//     return
-//   } else {
-//     var dataView = event.target.getAttribute('data-view');
-//     viewChanger(dataView)
-//   }
-// })
+document.addEventListener('click', function () {
+  // eslint-disable-next-line no-empty
+  if (!event.target.classList.contains('view-changer')) {
+
+  } else {
+    var dataView = event.target.getAttribute('data-view');
+    viewChanger(dataView);
+  }
+});
