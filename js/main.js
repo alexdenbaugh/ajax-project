4d1bbb2ca15df293acbe32cff0341fcea53103c7
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-04-16 06:01:48 -0700

    Create guards for incorrect inputs. Manipulate form entries' inputs when necessary (logarithms, trig). #1

--- js/main.js
+++ js/main.js
@@ -7,8 +7,7 @@ function getNewtonData(type, problem) {
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     data.calculator.result = xhr.response.result;
-    updateResult(xhr.status);
-    // console.log('loaded')
+    // updateResult(xhr.status);
   });
   xhr.send();
 }
@@ -23,21 +22,39 @@ $formCalculator.addEventListener('submit', function () {
   data.calculator.type = $formCalculator.elements.type.value;
   data.calculator.problem = $formCalculator.elements.problem.value;
   data.calculator.result = null;
-  // console.log(data);
-  // formatCheck();
+  // formatCheck()
   getNewtonData(data.calculator.type, data.calculator.problem);
 });
 
-function updateResult(xhr) {
-  if (xhr.status === 400) {
-    // $pResult.textContent = responses[data.calculator.type];
-    $pResult.classList.remove('black-text');
-    $pResult.classList.add('red-text');
-  } else {
-    $pResult.textContent = data.calculator.result;
-  }
-}
+// function updateResult(status) {
+//   if (status === 400 || (!data.calculator.result && data.calculator.result !== 0 && isNaN(data.calculator.result))) {
+//     $pResult.textContent = responses[data.calculator.type];
+//     $pResult.classList.remove('black-text');
+//     $pResult.classList.add('red-text');
+//   } else {
+//     $pResult.textContent = data.calculator.result;
+//   }
+// }
 
 // function formatCheck() {
-
+//   var problem = data.calculator.problem;
+//   var check = false;
+//   for (var i = 0; i < typeCheck.length; i++) {
+//     if (problem.startsWith(typeCheck[i])) {
+//       check = true;
+//       break
+//     }
+//   }
+//   if (check === false) {
+//     return
+//   }
+//   if (data.calculator.type === 'log') {
+//     problem = problem.slice(3);
+//     problem = problem.slice(0, problem.length - 1);
+//     problem = problem.replace('(', '|');
+//   } else if (data.calculator.type === 'trig') {
+//     data.calculator.type = problem.slice(0, problem.indexOf('('));
+//     problem = problem.slice(problem.indexOf('(') + 1, problem.length - 1)
+//   }
+//   data.calculator.problem = problem
 // }
