a71468e73dcde570d5f82b819e6a47ee0326150f
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-04-16 01:01:27 -0700

    Make data model in JS. #1

--- js/data.js
+++ js/data.js
@@ -1,1 +1,25 @@
 /* exported data */
+
+// var data = {
+//   calculator: {
+//     type: '',
+//     problem: '',
+//   },
+//   practice: {
+//     simplify: [],
+//     factor: [],
+//     derive: [],
+//     integrate: []
+//   }
+// }
+
+// var responses = {
+//   correct: 'Correct!',
+//   incorrect: 'Incorrect',
+//   simplify: 'Please ues "^" for exponents and use parenthesis when dividing polynomials',
+//   factor: 'Please ues "^" for exponents and use parenthesis when dividing polynomials',
+//   derive: 'Please ues "^" for exponents and use parenthesis when dividing polynomials',
+//   integrate: 'Please ues "^" for exponents and use parenthesis when dividing polynomials',
+//   trig: 'Please ues "^" for exponents and use parenthesis when dividing polynomials',
+//   log: 'Please ues "^" for exponents and use parenthesis when dividing polynomials'
+// }
