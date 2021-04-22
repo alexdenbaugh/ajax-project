7e47bb0fd108050af4ab1b0db542a96a9826e330
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-04-21 18:03:00 -0700

    In progress of creating the polynomial generator. #2

--- js/main.js
+++ js/main.js
@@ -104,3 +104,14 @@ document.addEventListener('click', function () {
 // function practiceProblem() {
 
 // }
+
+// function getPolynomial(maxSize) {
+//   var x = 'x';
+
+// }
+
+// function randomPositiveInteger(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
