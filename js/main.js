69cf5a0db90c8bb32bbf01824c75f3e85fc29495
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-05-17 00:51:44 -0700

    Fixed bug that writes current practice problem over all of the problems in data.progress. #5

--- js/main.js
+++ js/main.js
@@ -144,12 +144,16 @@ $nextQuestion.addEventListener('click', function (event) {
   if (event.target !== $nextQuestion) {
     return;
   }
-  // console.log('practice.type', data.practice.type)
-  // var saveProgress = {settings, type, problem, userAnswer, correctAnswer, result} = data.practice;
-  // console.log('saveprogress', saveProgress)
-  // data.progress[saveProgress.type].push(saveProgress);
-  // console.log('data.progress[data.practice.type]', data.progress[data.practice.type])
-  // console.log('data.progress', data.progress);
+  data.progress[data.practice.type].push(data.practice);
+  const saveSettings = data.practice.settings;
+  data.practice = {
+    settings: saveSettings,
+    type: '',
+    problem: '',
+    userAnswer: '',
+    correctAnswer: '',
+    result: null
+  };
   $practiceForm.elements.answer.value = '';
   $practiceSubmitButton.classList.remove('hidden');
   practiceProblem();
